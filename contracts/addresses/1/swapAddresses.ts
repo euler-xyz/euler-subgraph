bbce9edc50f703afa92d4820c9c6afb5fbfbc48b
damarnez <dani.elnota@gmail.com>
2025-04-10 21:48:01 +0200

    feat: swap logic

---
+++ contracts/addresses/1/swapAddresses.ts
@@ -0,0 +1,4 @@
+export const swapAddresses = {
+    eulerSwapFactory: "0x79d3a7a9d203d352a655255BdB1a233623f536B7",
+    eulerSwap: "0x0000000000000000000000000000000000000000",
+}
---
+++ src/factories/euler-swap-factory.ts
@@ -0,0 +1,10 @@
+
+import { PoolDeployed as PoolDeployedEvent, PoolUninstalled as PoolUninstalledEvent } from '../../generated/EulerSwapFactory/EulerSwapFactory'
+
+function handlePoolDeployed(event: PoolDeployedEvent): void {
+
+}
+
+function handlePoolUninstalled(event: PoolUninstalledEvent): void {
+
+}
